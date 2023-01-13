14e69b2629e2d59bfe44794481917ca1480f0f7d
djfir0 <djfir0@me.com>
2023-01-13 01:07:37 -0700

    2

--- .config/VSCodium/User/History/5955f5f8/Yyo5.rs
+++
@@ -1,8 +0,0 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
-
-fn main() {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(get_index))
-    });
-}
